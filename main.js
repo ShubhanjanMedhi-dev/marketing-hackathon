2ee08cc5301e5bf6194acc07b10f140690cae4fd
Shubhanjan Medhi <shubhanjan@squadcast.com>
2023-02-02 20:51:43 +0530

    updates

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const searchForm = document.querySelector("#search-form");
+const searchForm1 = document.querySelector("#search-form1");
 const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input");
+const searchFormInput1 = searchForm1.querySelector("input"); // <=> document.querySelector("#search-form1 input");
 const info = document.querySelector(".info");
 var title;
+var incidentId;
 
 // The speech recognition interface lives on the browser’s window object
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
@@ -55,15 +58,32 @@ if(SpeechRecognition) {
     }
     else if(!searchFormInput.value) {
       searchFormInput.value = transcript;
-      title = transcript;
-      getDescription();
+      if(searchFormInput.value == 'manage'){
+        searchForm1.style.display = 'block';
+      }
+      if(searchFormInput.value == 'acknowledge'){
+        acknowledgeIncident();
+      }
+      if(searchFormInput.value == 'resolve'){
+        resolveIncident();
+      }
+      //getDescription();
     }
     else {
       if(transcript.toLowerCase().trim()==="go") {
-        searchForm.submit();
+        //searchForm.submit();
       }
       else if(transcript.toLowerCase().trim()==="reset input") {
         searchFormInput.value = "";
+      } 
+      else if(searchFormInput.value == 'manage'){
+        searchForm1.style.display = 'block';
+      }
+      else if(searchFormInput.value == 'acknowledge'){
+        acknowledgeIncident();
+      }
+      else if(searchFormInput.value == 'resolve'){
+        resolveIncident();
       }
       else {
         searchFormInput.value = transcript;
@@ -76,7 +96,7 @@ if(SpeechRecognition) {
     // }, 500);
   }
   
-  info.textContent = 'Voice Commands: "stop recording", "reset input", "go"';
+  info.textContent = 'Voice Commands: "stop recording", "reset input", "manage", "acknowledge", "resolve"';
   
 }
 else {
@@ -85,6 +105,7 @@ else {
 }
 
 function getDescription(){
+    title = searchFormInput.value;
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -105,7 +126,9 @@ function getDescription(){
 
     fetch("https://prompts.riku.ai/webhook/run", requestOptions)
         .then(response => response.text())
-        .then(result => createIncident(result))
+        .then(result => createIncident(result.split('Response: ')[1])
+            //createIncident(result)
+        )
         .catch(error => console.log('error', error));
 }
 
@@ -128,6 +151,44 @@ function createIncident(desc){
     };
 
     fetch("https://api.squadcast.com/v2/incidents/api/f0d1e89ecdd07a6d966c0051b1b96c9f29caaf1c", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+searchFormInput1.onblur = function(){
+    searchForm1.style.display = 'none';
+    searchFormInput.value = "";
+    incidentId = searchFormInput1.value;
+}
+
+function acknowledgeIncident(){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzU1MTY1MzMsImp0aSI6IjkyMmQ2MzE3OWM2N2I4NWYwNDQ1OThkZTFmM2FjNjJkY2MxNTU5MWU5OTJkYjYzOTczMDc1NWZjOTQxOTcyNDEiLCJpYXQiOjE2NzUzNDM3MzMsImlzcyI6ImFwaS5zcXVhZGNhc3QuY29tIiwibmJmIjoxNjc1MzQzNzMzLCJpZCI6IjVkMTM1N2YxNmZmMmM2MDAxMGJjMzg1OCIsImZpcnN0X25hbWUiOiJTcXVhZGNhc3QiLCJlbWFpbCI6ImRlbW9Ac3F1YWQuY2FtcCIsInVzZXIiOm51bGwsInNzb19sb2dpbiI6ZmFsc2UsInNzb190b2tlbiI6IiIsIm9yZ2FuaXphdGlvbl9pZCI6IjYwY2IyMDBiYzlkZTRhMDAwOGQyZmM0MSIsIm9yZ2FuaXphdGlvbiI6bnVsbCwicmVmcmVzaF90b2tlbiI6Ijk3YTM0ODhmYmRiYmEzOTY2MzFmYzI1ZjFmMTMzNDU5ZmE4MmVjMmI5ODhkZTI0ODhhNzczYTYzMjgzYTNjODk0NjRlZjQ5M2JiNGI4MTMzNDVjODZhZmJhYTYwNjYwYzVkNDBlZWU0NTNjMjFjMjE2MDBhN2I5Mjc3MzE3NGNkIiwid2ViX3Rva2VuIjpmYWxzZSwiYXBpX3Rva2VuIjp0cnVlfQ.XFOQkvASxjLrqK2bOn5SBwVOLG7z-OyUntxtHUFB5PQ");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.squadcast.com/v3/incidents/"+incidentId+"/acknowledge", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+function resolveIncident(){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzU1MTY1MzMsImp0aSI6IjkyMmQ2MzE3OWM2N2I4NWYwNDQ1OThkZTFmM2FjNjJkY2MxNTU5MWU5OTJkYjYzOTczMDc1NWZjOTQxOTcyNDEiLCJpYXQiOjE2NzUzNDM3MzMsImlzcyI6ImFwaS5zcXVhZGNhc3QuY29tIiwibmJmIjoxNjc1MzQzNzMzLCJpZCI6IjVkMTM1N2YxNmZmMmM2MDAxMGJjMzg1OCIsImZpcnN0X25hbWUiOiJTcXVhZGNhc3QiLCJlbWFpbCI6ImRlbW9Ac3F1YWQuY2FtcCIsInVzZXIiOm51bGwsInNzb19sb2dpbiI6ZmFsc2UsInNzb190b2tlbiI6IiIsIm9yZ2FuaXphdGlvbl9pZCI6IjYwY2IyMDBiYzlkZTRhMDAwOGQyZmM0MSIsIm9yZ2FuaXphdGlvbiI6bnVsbCwicmVmcmVzaF90b2tlbiI6Ijk3YTM0ODhmYmRiYmEzOTY2MzFmYzI1ZjFmMTMzNDU5ZmE4MmVjMmI5ODhkZTI0ODhhNzczYTYzMjgzYTNjODk0NjRlZjQ5M2JiNGI4MTMzNDVjODZhZmJhYTYwNjYwYzVkNDBlZWU0NTNjMjFjMjE2MDBhN2I5Mjc3MzE3NGNkIiwid2ViX3Rva2VuIjpmYWxzZSwiYXBpX3Rva2VuIjp0cnVlfQ.XFOQkvASxjLrqK2bOn5SBwVOLG7z-OyUntxtHUFB5PQ");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.squadcast.com/v3/incidents/"+incidentId+"/resolve", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
