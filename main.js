9fe89d3a88b927b22dfb0239b41094363bc95e9b
Shubhanjan Medhi <shubhanjan@squadcast.com>
2023-02-02 15:50:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchForm = document.querySelector("#search-form");
 const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input");
 const info = document.querySelector(".info");
+var title;
 
 // The speech recognition interface lives on the browser’s window object
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
@@ -54,6 +55,8 @@ if(SpeechRecognition) {
     }
     else if(!searchFormInput.value) {
       searchFormInput.value = transcript;
+      title = transcript;
+      getDescription();
     }
     else {
       if(transcript.toLowerCase().trim()==="go") {
@@ -79,4 +82,53 @@ if(SpeechRecognition) {
 else {
   console.log("Your Browser does not support speech Recognition");
   info.textContent = "Your Browser does not support Speech Recognition";
+}
+
+function getDescription(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "Name": "Muthali Ganesh",
+        "Secret": "pBTzDfGt9QL3Mq5KE2BHpHXNP",
+        "Prompt ID": "1675327731925x936351740311371800",
+        "Input 1": title,
+        "n": 1
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://prompts.riku.ai/webhook/run", requestOptions)
+        .then(response => response.text())
+        .then(result => createIncident(result))
+        .catch(error => console.log('error', error));
+}
+
+function createIncident(desc){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "message": title,
+        "description": desc,
+        "status": "trigger",
+        "event_id": "3"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.squadcast.com/v2/incidents/api/f0d1e89ecdd07a6d966c0051b1b96c9f29caaf1c", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
